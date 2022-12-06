c9c3d4871133bf950975faf8d573028de5b820cf
Aleksandra <zharovaaleksis@gmail.com>
2022-12-06 19:57:57 +0300

    updated bonus at total

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int count = 100;
-        int amount = 1099;
+        int amount = 100;
         int bonus = amount / 100;
         int finalcount = count + amount;
         int total = 0;
         if (amount < 1000) {
             total = finalcount;
+            bonus = 0;
         } else {
             total = finalcount + bonus;
         }
